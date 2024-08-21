d0bfd1537ad58a03a8884d3243e727a8d7092df7
davicoenerosa@gmail.com <davicoenerosa@gmail.com>
2024-08-20 21:42:29 -0300

    Adicionando Hello World

---
+++ hello-world/src/main/java/br/com/davi/HelloWorld.java
@@ -0,0 +1,24 @@
+package br.com.davi;
+
+public class HelloWorld {
+
+	private final Long id;
+	
+	private final String name;
+
+	public HelloWorld(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+
+}
