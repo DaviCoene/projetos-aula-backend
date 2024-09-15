fff1109f80b8c5a797f03e10f2d54b0f8b6b73a7
Davi Coene Rosa <62859036+DaviCoene@users.noreply.github.com>
2024-09-15 20:08:54 -0300

    Delete rest-Book/src/main/java/br/com/julio directory

--- rest-Book/src/main/java/br/com/julio/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.julio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.julio.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long>{
-	
-}
